3c119ccbe44776ad0f4e59271ee874821c2a4a52
Chenyuan Zhu <chenyuanzhu@Chenyuans-MacBook-Pro.local>
2021-02-26 12:00:40 +0800

    update national view (not done)

--- src/nationalview.js
+++ src/nationalview.js
@@ -1,9 +1,10 @@
 import personalincome from '../static/income_per_hour_by_county.csv'
-import personalincomebystate from '../static/income_per_hour_by_state.csv'
+import personalincome2 from '../static/income_per_hour_by_county2.csv'
+
 "use strict"; 
 var countydata = [];
-var statedata = [];
-//var yearrange = ['2016','2017','2018','2019'];
+var countydata2 = [];
+var years = [2016,2017,2018,2019];
 const options = {
     config: {
     },
@@ -15,10 +16,6 @@ const options = {
     },
 };
 
-//const selectYear = vl.selectSingle('Select') // name the selection 'Select'
-//.fields('Year')          // limit selection to the Major_Genre field
-//.init('2016') // use first genre entry as initial value
-//.bind(vl.menu(yearrange));
 vl.register(vega, vegaLite, options);
 
 
@@ -26,32 +23,50 @@ d3.csv(personalincome).then(function(data2) {
   data2.forEach(function(d){
     countydata.push(d);
   })
-  d3.csv(personalincomebystate).then(function(data3){
+  d3.csv(personalincome2).then(function(data3){
     data3.forEach(function(e){
-      statedata.push(e);
+      countydata2.push(e);
     })
 
-    drawAnnualIncomeCounty('2019');
+    drawAnnualIncomeCounty();
   })
 });
 
 
 
-function drawAnnualIncomeCounty(year) {
-  vl.markGeoshape({stroke: '#aaa', strokeWidth: 0.25})
+function drawAnnualIncomeCounty() {  
+  const sel = vl.selectSingle();
+  //const selectyear = vl.selectSingle('Select') 
+    //        .fields('Year')          
+      //      .init({Year: years[0]}) 
+        //    .bind({Year: vl.slider(2016,2019,1)});
+  const map = vl.markGeoshape({stroke: 'lightgrey', strokeWidth: 0.25})
         .data(vl.topojson("https://cdn.jsdelivr.net/npm/vega-datasets@1.31.1/data/us-10m.json").feature('counties'))
+        .select(sel)
         .transform(
-          vl.lookup('id').from(vl.data(countydata).key('GeoFips').fields(['GeoName',year])),
+          vl.lookup('id').from(vl.data(countydata).key('GeoFips').fields(['2019','GeoName']))
         )
         .encode(
-          vl.color().fieldQ(year).scale({domain: [0, 100],scheme: 'lighttealblue'}),
-          vl.tooltip(['GeoName',year])
-        )
-        .project(vl.projection('albersUsa'))
-        .width(890).height(500)
+          vl.color().fieldQ('2019').scale({domain: [0, 80],scheme: 'tealblues'}),
+          vl.opacity().if(sel,vl.value(1)).value(0.5),
+          vl.tooltip(['2019','GeoName'])
+        );
+  
+  const states = vl.markGeoshape({fill: null, stroke: 'white', strokeWidth: 0.5})
+        .data(vl.topojson("https://cdn.jsdelivr.net/npm/vega-datasets@1.31.1/data/us-10m.json").mesh('states'));
+  const trend = vl.markPoint()
+                  .data(countydata2)
+                  //.select(sel)
+                  .encode(
+                    vl.x().fieldO('Year'),
+                    vl.y().average('Income'),
+                    //vl.opacity().if(sel,vl.value(1)).value(0),
+                    vl.tooltip('Income')
+                  )
+  return vl.hconcat(vl.layer(map,states).project(vl.projection('albersUsa')).width(800).height(400),trend)
         .config({view: {stroke: null}})
         .render()
-    .then(viewElement => {
-      document.getElementById('nv').appendChild(viewElement);
-  });
+        .then(viewElement => {
+            document.getElementById('nv').appendChild(viewElement);
+        });
 }
